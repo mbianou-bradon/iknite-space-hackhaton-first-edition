736c4579f9f67321d04c039b0656f6be865eda5b
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-04-02 14:49:53 +0100

    Removed ... file

--- src/Error.tsx
+++ src/Error.tsx
@@ -8,7 +8,7 @@ export default function Error() {
             <h3 className='text-6xl font-extrabold mb-5'>404</h3>
             <p className='text-2xl mb-6'>Page Not Found</p>
             
-            <NavLink to="/" className="w-fit px-6 py-2 bg-primary border border-primary hover:bg-white text-white hover:text-primary rounded-lg cursor-pointer">
+            <NavLink to="/" className="w-fit px-6 py-2 bg-primary border border-primary hover:bg-white text-slate-900 hover:text-primary rounded-lg cursor-pointer">
                 Goto Home
             </NavLink>
         </div>
