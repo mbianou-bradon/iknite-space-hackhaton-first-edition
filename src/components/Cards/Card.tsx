9d420871a5c2b53bd93a34b1e3e9a68cbc9ea10e
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-04-02 17:06:26 +0100

    Created SignIn and SignUp

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GrLocation } from "react-icons/gr"
+import { Link } from "react-router-dom"
 
 
 
@@ -8,33 +9,38 @@ interface Props{
 }
 
 
+
 export default function Card(prop:Props) {
   return (
     <div className="h-[24rem] w-[17rem] bg-white cursor-pointer hover:shadow-md">
-      <div>
-          <div className="h-[14rem] w-full bg-rose-600">
-            <img src={prop.img} className="h-full w-full object-cover object-center"/>
-          </div>
-          <div className="cardBody flex flex-col h-[10rem] px-4 pt-3 pb-3">
-            <div className="text-orange-500 mb-3 font-bold text-sm"><h2>NEGOTIABLE</h2></div>
-            <div className="cardName text-2xl font-semibold">
-              <h2>Name</h2>
+      <Link to="/cars/:carId">
+        <div>
+            <div className="h-[14rem] w-full bg-rose-600">
+              <img src={prop.img} className="h-full w-full object-cover object-center"/>
             </div>
-            <div className="flex items-baseline gap-2 my-auto">
-              <h2 className="text-sm">Price</h2>
-              <p className="text-xl font-bold"><span>500,000</span> FCFA</p>
+            <div className="cardBody flex flex-col h-[10rem] px-4 pt-3 pb-3">
+              <div className="text-orange-500 mb-3 font-bold text-sm"><h2>NEGOTIABLE</h2></div>
+              <div className="cardName text-2xl font-semibold">
+                <h2>Name</h2>
+              </div>
+              <div className="flex items-baseline gap-2 my-auto">
+                <h2 className="text-sm">Price</h2>
+                <p className="text-xl font-bold"><span>500,000</span> FCFA</p>
+              </div>
+              <div>
+                  <div className="flex items-center gap-1 font-bold">
+                      {/* Location Icon */}
+                      <div>
+                          <GrLocation />
+                      </div>
+                      <h2>Douala</h2>
+                  </div>
+              </div>
             </div>
-            <div>
-                <div className="flex items-center gap-1 font-bold">
-                    {/* Location Icon */}
-                    <div>
-                        <GrLocation />
-                    </div>
-                    <h2>Douala</h2>
-                </div>
-            </div>
-          </div>
-      </div>
+        </div>
+      </Link>
     </div>
   )
 }
+
+
